56bd6e9ff6f0239bc4846d2f77e4f01e97db6824
Keiko Kaneko <keiko.cda@gmail.com>
2020-10-11 20:40:46 +0900

    :nail_care: Add icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <main>
     <h1>neko314's site</h1>
-    <a href="https://github.com/neko314">GitHub</a>
-    <a href="https://neko314.hatenablog.com/">blog</a>
-    <a href="https://twitter.com/neko314_">Twitter</a>
+    <a href="https://github.com/neko314">
+      <font-awesome-icon :icon="['fab', 'github']" />GitHub
+    </a>
+    <a href="https://neko314.hatenablog.com/">
+      <font-awesome-icon icon="book-reader" />blog
+    </a>
+    <a href="https://twitter.com/neko314_">
+      <font-awesome-icon :icon="['fab', 'twitter']" />Twitter
+    </a>
   </main>
 </template>
