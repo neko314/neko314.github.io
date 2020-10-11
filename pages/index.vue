4bf9f8f588c2e41f6838af2bc26728352facab0d
Keiko Kaneko <keiko.cda@gmail.com>
2020-10-11 17:50:58 +0900

    Simple Home Page with nuxt

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <main>
+    <h1>neko314's site</h1>
+    <a href="https://github.com/neko314">GitHub</a>
+    <a href="https://neko314.hatenablog.com/">blog</a>
+    <a href="https://twitter.com/neko314_">Twitter</a>
+  </main>
+</template>
